3ee8e12d6444e5998c839f6e709c7056603a9898
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2022-12-23 10:58:50 +0000

    Copy /root volumes to non-root user dir

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -8,14 +8,16 @@ fi
 
 TF_ARCHIVE=$(find ${TF_ROOT_PATH} -name "terraform_${TF_RUNNER_VERSION}*.zip" -print)
 if [[ ${TF_ARCHIVE} == "" ]]; then
-  log-output error "Unsupported terraform version: ${TF_RUNNER_VERSION}"
-  exit 1
+    log-output error "Unsupported terraform version: ${TF_RUNNER_VERSION}"
+    exit 1
 fi
 
 if [[ $UID -ne 0 ]]; then
-  sudo -E unzip -q ${TF_ARCHIVE} -d ${TF_BIN_PATH}/
-  sudo -E chown -R tfrunner:tfrunner /home/tfrunner/ /src
+    sudo unzip -q ${TF_ARCHIVE} -d ${TF_BIN_PATH}/
+    sudo cp -aR /root/.aws /home/tfrunner/.aws
+    sudo cp -aR /root/.ssh /home/tfrunner/.ssh
+    sudo chown -R tfrunner:tfrunner /home/tfrunner/ /src
 else
-  unzip -q ${TF_ARCHIVE} -d ${TF_BIN_PATH}/
+    unzip -q ${TF_ARCHIVE} -d ${TF_BIN_PATH}/
 fi
 /usr/bin/run-terraform "$@"
